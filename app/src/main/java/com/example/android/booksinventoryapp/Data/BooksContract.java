250577c7209e1a25a5bc8ae0056f4e8e3b4e4f15
Cribuz73 <cribuz73@gmail.com>
2017-07-24 23:21:56 +0300

    final

--- app/src/main/java/com/example/android/booksinventoryapp/Data/BooksContract.java
+++ app/src/main/java/com/example/android/booksinventoryapp/Data/BooksContract.java
@@ -18,7 +18,6 @@ public final class BooksContract {
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-
     public static final String PATH_BOOKS = "books";
 
     public static final class BooksEntry implements BaseColumns {
